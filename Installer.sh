190f2c57413f050cd9e86c479d87156d517469a4
ALLRiPPED <37816162+ALLRiPPED@users.noreply.github.com>
2022-03-21 22:29:58 -0500

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 export NCURSES_NO_UTF8_ACS=1
 
-function installer-menu() { 
-local choice while true; 
-do choice=$(dialog --backtitle "$BACKTITLE" --title " Installer" \ --ok-label Select --cancel-label Back \ --menu "MoviePlayer Install Menu " 30 70 50 \
-1 "Install MoviePlayer" \
-3 "MoviePlayer Info.  " \
-2>&1 >/dev/tty) 
+function installer-menu() {
+local choice
+while true
+do choice=$(dialog --backtitle "$BACKTITLE" --title " Installer" \
+    --ok-label Select --cancel-label Back \
+    --menu "MoviePlayer Install Menu " 30 70 50 \
+    1 "Install MoviePlayer" \
+    2 "MoviePlayer Info.  " \
+    2>&1 >/dev/tty)
 
-case "$choice" in 
-1) install ;; 
-3) movie-info ;;
-*) break 
-;; esac done
+    case "$choice" in
+    1) install ;;
+    2) movie-info ;;
+    *) break ;;
+    esac
+  done
 }
-
 
 function install() {
 echo "Setting up Files and Folders"
@@ -30,41 +33,23 @@ sudo mkdir videos
 sudo chmod -R 777 videos
 cd
 sudo chown -R pi:pi /home/pi/RetroPie/roms
-cd /etc/emulationstation
-echo "Moving your es_systems.cfg to backups/es_systems.old"
 sleep 2
 clear
-mkdir ~/backups
-sudo mv es_systems.cfg ~/backups/es_systems.old
-clear 
---yesno "Would You Like To Overwrite ES Systems Config?" 0 0 )
-then edit-yes 
-else edit-no
-}
-
-function edit-yes() {
-wget . . . . . . . . .. . . es-systems.cfg
-echo "Installing MPV"
-sudo apt-get -y install mpv
-sleep 2
-echo " Install Complete! Rebooting Now" 
-sleep 5
-sudo reboot now
-}
-
-function edit-no() {
-echo "Installing MPV"
-sudo apt-get -y install mpv
-sleep 2
-echo " Install Complete! Rebooting Now" 
-sleep 5
-sudo reboot now
+if [ ! -s "/opt/retropie/configs/all/emulationstation/es_systems.cfg" ]; then sudo rm -f /opt/retropie/configs/all/emulationstation/es_systems.cfg; fi
+if [ ! -f "/opt/retropie/configs/all/emulationstation/es_systems.cfg" ]; then sudo cp /etc/emulationstation/es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg; sudo chown pi:pi /opt/retropie/configs/all/emulationstation/es_systems.cfg; else cp -f /opt/retropie/configs/all/emulationstation/es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg.old; fi
+CONTENT1="\t<system>\n\t\t<name>Videos</name>\n\t\t<fullname>Videos</fullname>\n\t\t<path>/home/pi/RetroPie/roms/videos</path>\n\t\t<extension>.avi .AVI .mp4 .MP4 .mov .MOV</extension>\n\t\t<command>/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ video %ROM%</command>\n\t\t<platform>videos</platform>\n\t\t<theme>videos</theme>\n\t</system>"
+C1=$(echo $CONTENT1 | sed 's/\//\\\//g')
+if grep -q Videos "/opt/retropie/configs/all/emulationstation/es_systems.cfg"; then echo "es_systems.cfg entry confirmed"
+else
+	sed "/<\/systemList>/ s/.*/${C1}\n&/" /opt/retropie/configs/all/emulationstation/es_systems.cfg > $HOME/temp
+	cat $HOME/temp > /opt/retropie/configs/all/emulationstation/es_systems.cfg
+	rm -f $HOME/temp
+fi
 }
 
 function movie-info() { 
 ### mesage box info ###
+echo "do somthine here!"
 }
 
 installer-menu
-
-
