fcf10bc1c9e99a61a8e4eea82be9948853420fc1
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-05-16 15:55:11 -0400

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -5,7 +5,7 @@ function installer-menu() {
 local choice
 while true
 do choice=$(dialog --backtitle "$BACKTITLE" --title "O.P.P Installer" \
-    --ok-label Select --cancel-label Back \
+    --ok-label Select --cancel-label Exit \
     --menu "O.P.P(Omx Player Pi ) Installer Menu " 0 0 \
     1 "Install O.P.P" \
     2 "O.P.P Info  " \
