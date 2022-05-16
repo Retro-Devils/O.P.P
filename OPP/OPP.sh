ddeea9ec708f572153f6f53dcc10c0117e608be4
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-05-16 08:17:35 -0600

    Update OPP.sh

--- OPP/OPP.sh
+++ OPP/OPP.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$1"> /dev/shm/temp.vid
-sudo /home/pi/RetroPie-Setup/retropie_packages.sh retropiemenu launch $HOME/OPP/play.sh
+sudo $HOME/RetroPie-Setup/retropie_packages.sh retropiemenu launch $HOME/OPP/play.sh
