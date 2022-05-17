87842c7112c5f0eb328dc2f6d2d27d474c6e0af1
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-05-17 05:28:59 -0600

    Update OPP.sh

--- OPP/OPP.sh
+++ OPP/OPP.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+if [ -f "$HOME/RetroPie/retropiemenu/imp/Pause.sh" ]; then
+bash "$HOME"/RetroPie/retropiemenu/imp/Pause.sh
+sleep 1 
+echo "$1"> /dev/shm/temp.vid
+sudo /home/pi/RetroPie-Setup/retropie_packages.sh retropiemenu launch $HOME/OPP/play.sh
+else
 echo "$1"> /dev/shm/temp.vid
 sudo $HOME/RetroPie-Setup/retropie_packages.sh retropiemenu launch $HOME/OPP/play.sh
+fi
