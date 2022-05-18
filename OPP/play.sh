500f92c553b9e3c1c5027bafc06f2291c309c0f2
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-05-17 18:02:51 -0700

    Update play.sh

--- OPP/play.sh
+++ OPP/play.sh
@@ -1,6 +1,2 @@
 omxplayer -o alsa "$(cat /dev/shm/temp.vid)" > /dev/null 2>&1
-if [ -f "$HOME/RetroPie/retropiemenu/imp/Play.sh" ]; then
-bash "$HOME"/RetroPie/retropiemenu/imp/Play.sh
-else
-sleep 1
-fi
+pkill -CONT mpg123 > /dev/null 2>&1
