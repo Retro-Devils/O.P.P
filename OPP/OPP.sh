113251781a67b2402fba129327bb56c1419220d4
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-05-23 07:20:06 -0600

    Update OPP.sh

--- OPP/OPP.sh
+++ OPP/OPP.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 pkill -STOP mpg123 > /dev/null 2>&1
 echo "$1"> /dev/shm/temp.vid
-omxplayer -o alsa "$HOME"/OPP/splash.mp4 > /dev/null 2>&1
 sudo $HOME/RetroPie-Setup/retropie_packages.sh retropiemenu launch $HOME/OPP/play.sh
