cdc6a071e61a0d59c9ee9d5e95b769ebb4b45df3
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-08-28 19:00:51 -0700

    Update OPP.sh

--- OPP/OPP.sh
+++ OPP/OPP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pkill -STOP mpg123 > /dev/null 2>&1
+pkill -f -STOP BGM.py
 echo "$1"> /dev/shm/temp.vid
 sudo $HOME/RetroPie-Setup/retropie_packages.sh retropiemenu launch $HOME/OPP/play.sh
